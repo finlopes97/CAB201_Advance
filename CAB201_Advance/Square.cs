a8cb968d834707d79615650534e74746d4774635
Kyle <kyle.finlayson1997@gmail.com>
2023-05-07 15:51:56 +1000

    Added "square" and "piece" barebones classes, amended file contents property occupadoPor now in Square class where it belongs.

---
+++ CAB201_Advance/Square.cs
@@ -0,0 +1,12 @@
+namespace CAB201_Advance;
+
+public class Square
+{
+    private char occupadoPor;
+    
+    public char OccupadoPor
+    {
+        get => occupadoPor;
+        set => occupadoPor = value;
+    }
+}
